153eea1749d9e89f3a6206a2ac02cd8f27c9fb42
@erick <erick.hx22@gmail.com>
2020-08-27 13:48:23 -0500

    chore: implement initial react app (#1)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { FormTable2 } from "./components";
+
+function App() {
+  return (
+    <div className="Container">
+      <FormTable2 />
+    </div>
+  );
+}
+
+export default App;
